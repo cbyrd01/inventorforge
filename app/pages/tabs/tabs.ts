3e1bdd92c01aaf2ed44c88b9d848af585a2bb34e
Christopher Byrd <cbyrd01@users.noreply.github.com>
2018-08-28 19:46:39 -0500

    Nuked door page

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {SignPage} from '../sign/sign';
-import {DoorPage} from '../door/door';
 import {AboutPage} from '../about/about';
 
 @Component({
@@ -10,13 +9,11 @@ export class TabsPage {
 
   private tab1Root: any;
   private tab2Root: any;
-  private tab3Root: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = SignPage;
-    this.tab2Root = DoorPage;
-    this.tab3Root = AboutPage;
+    this.tab2Root = AboutPage;
   }
 }
